320d2d07510b947ebd7d6f5b14f17207d2ff6753
adv100500 <121932525+adv100500@users.noreply.github.com>
2023-04-17 20:01:30 +0200

    Create NFTsimple.sol

---
+++ NFTsimple.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract DotaSIMPLE is ERC721URIStorage, ERC721Enumerable, Ownable {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+
+    }
+  
+    function awardItem(address player, string[] memory tokenURIlink) public onlyOwner //returns (uint256)                
+    {
+        for (uint i = 0; i < tokenURIlink.length; i++) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(player, newItemId);
+        _setTokenURI(newItemId, tokenURIlink[i]);
+        console.log('Solidity URI: ',tokenURIlink[i]);
+        console.log('Solidity ID: ',newItemId);
+        //return newItemId;
+        }
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721,ERC721URIStorage) returns (string memory){
+        return super.tokenURI(tokenId);
+    }
+
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721,ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+    function _burn(uint256 tokenId) internal virtual override(ERC721,ERC721URIStorage) {
+        super._burn(tokenId);
+    }    
+
+        function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override(ERC721,ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+
+    }
+
+}
