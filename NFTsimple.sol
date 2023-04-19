b3401996bcdf23838073b2abcfe5d0781112701d
adv100500 <121932525+adv100500@users.noreply.github.com>
2023-04-19 10:20:27 +0200

    Update NFTsimple.sol

--- NFTsimple.sol
+++ NFTsimple.sol
@@ -5,43 +5,29 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract DotaSIMPLE is ERC721URIStorage, ERC721Enumerable, Ownable {
-
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
-
-    }
-  
-    function awardItem(address player, string[] memory tokenURIlink) public onlyOwner //returns (uint256)                
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+    function awardItem(address player, string[] memory tokenURIlink) public onlyOwner              
     {
         for (uint i = 0; i < tokenURIlink.length; i++) {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(player, newItemId);
-        _setTokenURI(newItemId, tokenURIlink[i]);
-        console.log('Solidity URI: ',tokenURIlink[i]);
-        console.log('Solidity ID: ',newItemId);
-        //return newItemId;
+            _tokenIds.increment();
+            uint256 newItemId = _tokenIds.current();
+            _safeMint(player, newItemId);
+            _setTokenURI(newItemId, tokenURIlink[i]);
         }
     }
-
     function tokenURI(uint256 tokenId) public view virtual override(ERC721,ERC721URIStorage) returns (string memory){
         return super.tokenURI(tokenId);
     }
-
-
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721,ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
     function _burn(uint256 tokenId) internal virtual override(ERC721,ERC721URIStorage) {
         super._burn(tokenId);
     }    
-
         function _beforeTokenTransfer(
         address from,
         address to,
@@ -49,7 +35,5 @@ contract DotaSIMPLE is ERC721URIStorage, ERC721Enumerable, Ownable {
         uint256 batchSize
     ) internal virtual override(ERC721,ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
-
     }
-
 }
